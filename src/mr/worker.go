9f45cd744fc10333275615290e65be979505279f
wz <2945744494@qq.com>
2024-03-21 12:46:06 +0800

    123

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -194,6 +194,7 @@ func CallExample() {
 //
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	//c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
+	sockname := coordinatorSock()
 	c, err := rpc.DialHTTP("unix", sockname)
 	if err != nil {
 		log.Fatal("dialing:", err)
